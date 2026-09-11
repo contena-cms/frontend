0f4c391c31fd035636ce006d2d39ab4054cebaec
goxs <lyricsong47@gmail.com>
2026-09-11 10:31:55 +0800

    feat(frontend): sync media gallery accessibility updates

--- Resources/views/components/Ct/Media/Gallery.js
+++ Resources/views/components/Ct/Media/Gallery.js
@@ -2,7 +2,7 @@ export default class MediaGallery extends ContenaComponent {
     static options = {
         showMagnifier: true,
         thumbnailNavigationPosition: 'left',
-        thumbnailNavShowsMediaOnHover: true,
+        thumbnailNavShowsMediaOnHover: false,
         showNavigationArrows: true,
         showFullScreenGallery: true,
         zoomScale: 2.5,
