adc17343697eda5317a5e57f40e536c89b9a5fa2
goxs <lyricsong47@gmail.com>
2026-09-13 10:36:27 +0800

    fix(content-system): sync latest diagnostics and cache invalidation

--- DependencyInjection/content-system.php
+++ DependencyInjection/content-system.php
@@ -19,6 +19,12 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->parameters()
+        ->set('contena.content_system.section_assignment_entities', [
+            'header_content_layout' => 'header',
+            'footer_content_layout' => 'footer',
+        ]);
+
     $services = $containerConfigurator->services();
 
     // Entity Definitions
