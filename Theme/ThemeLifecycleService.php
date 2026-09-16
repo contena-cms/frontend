ccd12ea238ea40fd2440c16407d3ac6e967fd457
goxs <lyricsong47@gmail.com>
2026-09-16 11:32:23 +0800

    feat(core): add repository primary key payloads

--- Theme/ThemeLifecycleService.php
+++ Theme/ThemeLifecycleService.php
@@ -112,7 +112,7 @@ final class ThemeLifecycleService
         /** @var Criteria<array<string, string>> $parentCriteria */
         $parentCriteria = new Criteria();
         $parentCriteria->addFilter(new EqualsFilter('childId', $themeData['id']));
-        $toDeleteIds = $this->themeChildRepository->searchIds($parentCriteria, $context)->getIds();
+        $toDeleteIds = $this->themeChildRepository->searchIds($parentCriteria, $context)->getPrimaryKeyData();
         $this->themeChildRepository->delete($toDeleteIds, $context);
         $this->themeChildRepository->upsert($parentThemes, $context);
 
